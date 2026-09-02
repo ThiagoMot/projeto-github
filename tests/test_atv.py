dd5f67b0f32b88522540ddbec79ae0ed1625e49d
Thiago Monteiro <thiagomot.2003@gmail.com>
2026-09-02 16:37:18 -0300

    test: induz falha intencional para validar travamento do pipeline

--- tests/test_atv.py
+++ tests/test_atv.py
@@ -10,7 +10,7 @@ class TestSistemaBiblioteca(unittest.TestCase):
 
     def test_cadastrar_usuario(self):
         sucesso, _ = atv.cadastrar_usuario("1", "Thiago")
-        self.assertTrue(sucesso)
+        self.assertFalse(sucesso)
         self.assertIn("1", atv.usuarios)
 
         # Teste de duplicidade
